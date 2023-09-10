37e1ea9bc3ed6f3acb2d88e729a2b0823d424804
Martin Håkansson <martinhakansson@yahoo.com>
2023-09-10 13:05:35 +0200

    Support for msg 1067 added

---
+++ src/msg/msg1067.rs
@@ -0,0 +1,30 @@
+msg! (
+    id: msg1067_sat,
+    type_name: Msg1067Sat,
+    fields: [
+        (glo_satellite_id, df_u5),
+        (ssr_ura_class, df_u3),
+        (ssr_ura_value, df_u3)
+    ],
+);
+
+frag_vec_with_len!(
+    id: msg1067_sat_vec,
+    frag_id: msg1067_sat,
+    cap: SAT_CAP_1067, 63,
+    len_bits: 6,
+);
+
+msg!(
+    id: msg1067,
+    type_name: Msg1067T,
+    fields: [
+        (glo_epoch_time_s, df_u17),
+        (ssr_update_interval_index, df_u4),
+        (multiple_message_flag, df_flag),
+        (iod_ssr, df_u4),
+        (ssr_provider_id, df_u16),
+        (ssr_solution_id, df_u4),
+        (satellites, msg1067_sat_vec)
+    ],
+);
