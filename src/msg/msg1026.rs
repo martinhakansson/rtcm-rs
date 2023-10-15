2c6233eeb8e4f7ac125390ed839ffd03f2432df0
Martin Håkansson <martinhakansson@yahoo.com>
2023-10-16 00:05:53 +0200

    Support added for msg 1026

---
+++ src/msg/msg1026.rs
@@ -0,0 +1,15 @@
+msg!(
+    id: msg1026,
+    type_name: Msg1026T,
+    fields: [
+        (system_id,df_u8),
+        (projection_type_ind,df_u6),
+	    (la_fo_deg,df176_8_9),
+	    (lo_fo_deg,df177),
+	    (la_sp1_deg,df176_8_9),
+	    (la_sp2_deg,df176_8_9),
+	    (efo_m,df174_80),
+	    (nfo_m,df175_81)
+    ],
+);
+
