e22d289a2fee6d6c4c09d7f9f74ade3fb0637319
Martin Håkansson <martinhakansson@yahoo.com>
2023-11-19 19:29:40 +0100

    Support for msg 1017 added

--- src/msg/mod.rs
+++ src/msg/mod.rs
@@ -824,6 +824,7 @@ include_msg!(msg1013, "msg1013");
 include_msg!(msg1014, "msg1014");
 include_msg!(msg1015, "msg1015");
 include_msg!(msg1016, "msg1016");
+include_msg!(msg1017, "msg1017");
 include_msg!(msg1019, "msg1019");
 include_msg!(msg1020, "msg1020");
 include_msg!(msg1021, "msg1021");
---
+++ src/msg/msg1017.rs
@@ -0,0 +1,33 @@
+msg! (
+    id: msg1017_sat,
+    type_name: Msg1017Sat,
+    fields: [
+        (gps_satellite_id, df_u6),
+        (amb_status_ind,df_u2),
+        (non_sync_count,df_u3),
+        (geom_phase_diff_m,df069_70),
+        (iode,df_u8),
+        (iono_phase_diff_m,df069_70)
+    ],
+);
+
+frag_vec_with_len!(
+    id: msg1017_sat_vec,
+    frag_id: msg1017_sat,
+    cap: SAT_CAP_1017, 15,
+    len_bits: 4,
+);
+
+msg!(
+    id: msg1017,
+    type_name: Msg1017T,
+    fields: [
+        (network_id,df_u8),
+        (subnetwork_id,df_u4),
+        (gps_epoch_time_s,df065),
+        (multiple_message_flag,df_flag),
+        (ma_reference_station_id,df_u12),
+        (aux_reference_station_id,df_u12),
+        (satellites,msg1017_sat_vec)
+    ],
+);
