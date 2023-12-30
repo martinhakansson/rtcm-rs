00841e18616f05fd2b3bce659b3066bfd132eab6
Martin Håkansson <martinhakansson@yahoo.com>
2023-12-31 00:31:54 +0100

    Added support for msg 1009

---
+++ src/msg/msg1009.rs
@@ -0,0 +1,34 @@
+msg! (
+    id: msg1009_sat,
+    type_name: Msg1009Sat,
+    fields: [
+        (glo_satellite_id, df_u6),
+        (glo_l1_code_ind, df_flag),
+        (glo_satellite_freq_chan_number, df040),
+        (l1_pseudorange_m, df041),
+        (l1_phase_pseudorange_diff_m, df012_18_42_48),
+        (l1_lock_time_index, df_u7)
+    ],
+);
+
+frag_vec!(
+    id: msg1009_sat_vec,
+    frag_id: msg1009_sat,
+    cap_name: SAT_CAP_LEGACY,
+);
+
+msg_len_middle!(
+    id: msg1009,
+    type_name: Msg1009T,
+    fields1: [
+        (reference_station_id, df003),
+        (glo_epoch_time_ms, df_u27),
+        (synchronous_gnss_msg_flag, df005)
+    ],
+    len_field: df_leg_sat_len,
+    fields2: [
+        (divergence_free_smoothing_flag, df_flag),
+        (smoothing_interval_index, df_u3)
+    ],
+    vec_field: satellites, msg1009_sat_vec,
+);
