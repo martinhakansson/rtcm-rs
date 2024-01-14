a9febd6196cddbb48e352f2ebdca7187780e7cfb
Martin Håkansson <martinhakansson@yahoo.com>
2024-01-14 17:00:10 +0100

    Message 1001 revised

--- src/lib.rs
+++ src/lib.rs
@@ -45,23 +45,24 @@
 //!             reference_station_id: 100,
 //!             gps_epoch_time_ms: 0,
 //!             synchronous_gnss_msg_flag: 0,
-//!             satellites_len: 2,
-//!             gps_divergence_free_smoothing_flag: 0,
-//!             gps_smoothing_interval_bitval: 0,
-//!             satellites:  {
+//!             divergence_free_smoothing_flag: 0,
+//!             smoothing_interval_index: 0,
+//!             satellites: {
 //!                 let mut satellites = DataVec::new();
 //!                 satellites.push(Msg1001Sat {
 //!                     gps_satellite_id: 20,
 //!                     gps_l1_code_ind: 0,
-//!                     gps_l1_pseudorange_m: Some(20000000.0),
-//!                     gps_l1_phase_pseudorange_diff_m: Some(0.2),
-//!                     gps_l1_lock_time_bitval: 0 });
+//!                     l1_pseudorange_m: Some(20000000.0),
+//!                     l1_phase_pseudorange_diff_m: Some(0.2),
+//!                     l1_lock_time_index: 0,
+//!                 });
 //!                 satellites.push(Msg1001Sat {
 //!                     gps_satellite_id: 21,
 //!                     gps_l1_code_ind: 0,
-//!                     gps_l1_pseudorange_m: Some(26000000.0),
-//!                     gps_l1_phase_pseudorange_diff_m: Some(0.4),
-//!                     gps_l1_lock_time_bitval: 0 });
+//!                     l1_pseudorange_m: Some(26000000.0),
+//!                     l1_phase_pseudorange_diff_m: Some(0.4),
+//!                     l1_lock_time_index: 0,
+//!                 });
 //!                 satellites
 //!             }}));
 //!
--- src/msg/msg1001.rs
+++ src/msg/msg1001.rs
@@ -4,28 +4,30 @@ msg! (
     fields: [
         (gps_satellite_id, df009),
         (gps_l1_code_ind, df010),
-        (gps_l1_pseudorange_m, df011),
-        (gps_l1_phase_pseudorange_diff_m, df012_18_42_48),
-        (gps_l1_lock_time_bitval, df013)
+        (l1_pseudorange_m, df011),
+        (l1_phase_pseudorange_diff_m, df012_18_42_48),
+        (l1_lock_time_index, df013)
     ],
 );
 
 frag_vec!(
     id: msg1001_sat_vec,
     frag_id: msg1001_sat,
-    cap_name: DF006_CAP,
+    cap_name: SAT_CAP_LEGACY,
 );
 
-msg!(
+msg_len_middle!(
     id: msg1001,
     type_name: Msg1001T,
-    fields: [
+    fields1: [
         (reference_station_id, df003),
         (gps_epoch_time_ms, df004),
-        (synchronous_gnss_msg_flag, df005),
-        (satellites_len, df006),
-        (gps_divergence_free_smoothing_flag, df007),
-        (gps_smoothing_interval_bitval, df008),
-        (satellites, msg1001_sat_vec, satellites_len)
+        (synchronous_gnss_msg_flag, df005)
     ],
+    len_field: df_leg_sat_len,
+    fields2: [
+        (divergence_free_smoothing_flag, df007),
+        (smoothing_interval_index, df008)
+    ],
+    vec_field: satellites, msg1001_sat_vec,
 );
