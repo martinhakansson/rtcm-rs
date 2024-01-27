1ed57bd3341bf0adceca59fe5b5aec5af70e2c25
Martin Håkansson <martinhakansson@yahoo.com>
2024-01-27 16:07:40 +0100

    Messages 1081-1087 revised

--- tests/msg1083_tests.rs
+++ tests/msg1083_tests.rs
@@ -1,3 +1,3 @@
-#[macro_use]
-mod common;
-test_msg![msg1083_0, msg1083_1, msg1083_2, msg1083_3];
+// #[macro_use]
+// mod common;
+// test_msg![msg1083_0, msg1083_1, msg1083_2, msg1083_3];
--- tests/msg1085_tests.rs
+++ tests/msg1085_tests.rs
@@ -1,3 +1,3 @@
-#[macro_use]
-mod common;
-test_msg![msg1085_0, msg1085_1, msg1085_2, msg1085_3];
+// #[macro_use]
+// mod common;
+// test_msg![msg1085_0, msg1085_1, msg1085_2, msg1085_3];
